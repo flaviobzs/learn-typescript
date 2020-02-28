d3faeb9d7ee20499778e7b9610227a99a0482e97
Flavio Bezerra dos Santos <flavio.bezerra@seplag.al>
2020-02-28 14:37:06 -0300

    compile

---
+++ compilador/compilador.js
@@ -0,0 +1,23 @@
+"use strict";
+var canal = 'Gaveta';
+var inscritos = 610234;
+// canal = inscritos
+console.log("Canal = " + canal);
+// let nome = 'Pedro'
+function soma(a, b) {
+    return a + b;
+}
+var qualquerCoisa;
+qualquerCoisa = 12;
+qualquerCoisa = 'abc';
+function saudar(isManha) {
+    var saudacao;
+    if (isManha) {
+        saudacao = 'Bom Dia!';
+    }
+    else {
+        saudacao = 'Tenha uma boa vida!';
+    }
+    return saudacao;
+}
+//# sourceMappingURL=compilador.js.map
